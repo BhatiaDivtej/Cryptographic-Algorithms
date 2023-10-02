d4ced9eecd80f41b3484108878fa1fc6454a9bc5
BhatiaDivtej <divtej17@connect.hku.hk>
2023-10-02 23:20:49 +0800

    Task 1 Done

--- task1/decrypt_message.py
+++ task1/decrypt_message.py
@@ -10,9 +10,10 @@ return m: the plain text
 def decrypt_message(N, e, d, c):
     m = 0
     # TODO: Implement this function for Task 1
+    m = pow(c, d, N)  # Calculate c^d mod N
     return hex(m).rstrip('L')
 
 def get_student_number():
     # TODO: Fill your student number here
-    return ""
+    return "3035832438"
     
