d0cb6c338f524aa90efaf941041209d45c74a68b
BhatiaDivtej <divtej17@connect.hku.hk>
2023-10-02 23:55:09 +0800

    Task 3 Done

--- task3/get_private_key.py
+++ task3/get_private_key.py
@@ -13,7 +13,15 @@ return result: True if n1 and n2 share the same prime number; otherwise, returns
 def is_waldo(n1, n2):
     result = False
     # TODO: Implement this function for Task 3
-    return result
+
+    # Calculate the GCD of n1 and n2
+    gcd_result = gcd(n1, n2)
+
+    # they share a common prime factor if gcd result > 1
+    if gcd_result > 1:
+        return True
+    else:
+        return False    
 
 """
 This function will be called if is_waldo(n1, n2) returns True.
@@ -25,11 +33,28 @@ e: the encryption exponent
 return d: the decryption exponent
 """
 def get_private_key_from_n1_n2_e(n1, n2, e):
-    d = 0
-    # TODO: Implement this function for Task 3
+    # Calculate the GCD of n1 and n2
+    gcd_result = gcd(n1, n2)
+
+    # # Ensure that n1 and n2 share a common prime factor
+    # if gcd_result == 1:
+    #     raise ValueError("n1 and n2 do not share a common prime factor")
+
+    # Calculate p and q, the prime factors of n1
+    p = gcd_result
+    q = n1 // gcd_result
+
+    # Calculate phi(n1), where n1 = p * q
+    phi_n1 = (p - 1) * (q - 1)
+
+    # Calculate the modular multiplicative inverse of e modulo phi(n1)
+    d = pow(e, -1, phi_n1)
+
     return d
+
+
 
 def get_student_number():
     # TODO: Fill your student number here
-    return ""
+    return "3035832438"
     
