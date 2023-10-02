f839e859234fcebc27a5c9dcc6642df5679d2b97
BhatiaDivtej <divtej17@connect.hku.hk>
2023-10-02 23:39:06 +0800

    Task 2 done

--- task2/short_key.py
+++ task2/short_key.py
@@ -6,7 +6,14 @@ return (p, q): the factors of N
 def get_factors(N):
     # TODO: Implement this function for Task 2
     p, q = 0, 0
+    
     # TODO: Implement this function for Task 2
+    for i in range(2, int(N**0.5) + 1):
+        if N % i == 0:
+            p = i
+            q = N // i
+            break
+
     return p, q
 
 """
@@ -18,9 +25,11 @@ return d: the decryption exponent
 def get_private_key_from_p_q_e(p, q, e):
     d = 0
     # TODO: Implement this function for Task 2
+    d = pow(e, -1, ((p - 1) * (q - 1)))
     return d
+
 
 def get_student_number():
     # TODO: Fill your student number here
-    return ""
+    return "3035832438"
     
